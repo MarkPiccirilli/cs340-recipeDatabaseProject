7780ab39f7239f5d63fd35e3328608f3703fa7b0
Mark Piccirilli <mpicc704@gmail.com>
2018-07-20 17:21:31 -0400

    DDQ updated. new DMQ. html folder containing handlebars files and static html pages

--- DDQ.sql
+++ DDQ.sql
@@ -3,27 +3,38 @@
     Title: Data Definition Queries for Recipe Project
     Author: Mark Piccirilli
     Assignment: cs340 Project Step 2
-    Date: 7/10/18
+    Date: 7/18/18
 
 */
 
--- Create the ingrediants table
+-- If necessary Drop tables
+
+DROP TABLE IF EXISTS `recipe_ingredients`;
+DROP TABLE IF EXISTS `recipe_cookware`;
+DROP TABLE IF EXISTS `saved_recipes`;
+DROP TABLE IF EXISTS `user_contributor`;
+DROP TABLE IF EXISTS `recipes`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `cookware`;
 DROP TABLE IF EXISTS `ingredients`;
+
+-- Create the ingrediants table
 
 CREATE TABLE `ingredients` (
     `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    `calories` int(11) NOT NULL,
-    `fat` int(11) NOT NULL,
-    `sodium` int(11) NOT NULL,
-    `sugar` int(11) NOT NULL,
-    `protein` int(11) NOT NULL,
-    `vitaminA` int(11) NOT NULL,
-    `vitaminC` int(11) NOT NULL
+    `serving_size` decimal(19,1) NOT NULL,
+    `serving_size_unit` varchar(255),
+    `calories` decimal(19,1) NOT NULL,
+    `fat_g` decimal(19,1) NOT NULL,
+    `sodium_mg` decimal(19,1) NOT NULL,
+    `sugar_g` decimal(19,1) NOT NULL,
+    `protein_g` decimal(19,1) NOT NULL,
+    `vitaminA_pdv` int(11) NOT NULL,
+    `vitaminC_pdv` int(11) NOT NULL
 )ENGINE=InnoDB;
 
 -- Create the cookware table
-DROP TABLE IF EXISTS `cookware`;
 
 CREATE TABLE `cookware` (
     `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -33,21 +44,18 @@ CREATE TABLE `cookware` (
 
 
 -- Create the users table
-DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
     `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `firstName` varchar(255) NOT NULL,
-    `lastName` varchar(255) NOT NULL,
-    `userName` varchar(255) NOT NULL,
-    `password` char(20) NOT NULL,
+    `first_name` varchar(255) NOT NULL,
+    `last_name` varchar(255) NOT NULL,
+    `user_name` varchar(255) NOT NULL,
+    `user_password` varchar(255) NOT NULL,
     `email` varchar(255) NOT NULL,
-    `cookingExperience` varchar(5000)
+    `cooking_experience` varchar(5000)
 )ENGINE=InnoDB;
 
 -- Create the Recipes table
-
-DROP TABLE IF EXISTS `recipes`;
 
 CREATE TABLE `recipes` (
     `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -55,25 +63,23 @@ CREATE TABLE `recipes` (
     `instructions` varchar(8000) NOT NULL,
     `meal_type` varchar(255) DEFAULT NULL,
     `ethnic_cuisine` varchar(255) DEFAULT NULL,
+    `servings` int(11) NOT NULL,
     `contributor` int(11),
     `date_contributed` date NOT NULL,
     FOREIGN KEY (`contributor`)REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 )ENGINE=InnoDB;
 
 -- Create the user contributor table
-DROP TABLE IF EXISTS `user_contributor`;
 
 CREATE TABLE `user_contributor` (
-    `user_id` int(11),
+    `follower_id` int(11),
     `contributor_id` int(11),
-    PRIMARY KEY (`user_id`, `contributor_id`),
+    PRIMARY KEY (`follower_id`, `contributor_id`),
     FOREIGN KEY (`contributor_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
-    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
+    FOREIGN KEY (`follower_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
 )ENGINE=InnoDB;
 
 -- Create the save recipes table
-
-DROP TABLE IF EXISTS `saved_recipes`;
 
 CREATE TABLE `saved_recipes` (
     `recipe_id` int(11),
@@ -85,8 +91,6 @@ CREATE TABLE `saved_recipes` (
 
 -- create recipe cookware table
 
-DROP TABLE IF EXISTS `recipe_cookware`;
-
 CREATE TABLE `recipe_cookware`(
     `recipe_id` int(11),
     `cookware_id` int(11),
@@ -97,12 +101,58 @@ CREATE TABLE `recipe_cookware`(
 
 -- create recipe ingredients table
 
-DROP TABLE IF EXISTS `recipe_ingredients`;
-
 CREATE TABLE `recipe_ingredients` (
     `recipe_id` int(11),
-    `ingredients_id` int(11),
-    PRIMARY KEY(`recipe_id`, `ingredients_id`),
+    `ingredient_id` int(11),
+    `ingredient_quantity` DECIMAL(19,4),
+    PRIMARY KEY(`recipe_id`, `ingredient_id`),
     FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON UPDATE CASCADE,
-    FOREIGN KEY (`ingredients_id`) REFERENCES `ingredients` (`id`) ON UPDATE CASCADE
+    FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON UPDATE CASCADE
 )ENGINE=InnoDB;
+
+
+-- Insert sample Data
+
+-- sample users
+INSERT INTO users (first_name, last_name, user_name, user_password, email, cooking_experience) VALUES
+('Mark', 'Piccirilli', 'MarkPiccirilli', SHA1('1234'), 'piccirim@oregonstate.edu', 'I love to cook at home!'),
+('Leo', 'Greene', 'LeoTheCat', SHA1('ILoveSleeping'), 'LeoCat@gmail.com', NULL),
+('Jerome', 'McElroy', 'Chef', SHA1('chef1234'), 'chef@sounthparkelementry.com', 'cooking in the cafiteria at south park!'),
+('Spongebob', 'Squarepants', 'spongebob', SHA1('krabbypatties'), 'spongebob@krustykrab.com', 'Fry cook at the krusty krab, home of the krabby patty!!!');
+
+-- sample recipes
+INSERT INTO recipes (name, instructions, meal_type, ethnic_cuisine, servings, contributor, date_contributed) VALUES
+('chili', 'Add ground beef, kidney beans, onion, and tomato sauce to crock pot and cook on low for 6-8 hrs', NULL, NULL, 3, 3, '2001-7-11'),
+('Krabby Patty', 'Its Mr. Krabs sceret recipe', NULL, NULL, 1, 4, '2006-1-1'),
+('Lemonade', 'cut lemons and sqeeze into glass, add sugar and ice and water, shake', NULL, NULL, 2, 1, '2018-7-18');
+
+-- sample ingredients
+INSERT INTO ingredients (name, serving_size, serving_size_unit, calories, fat_g, sodium_mg, sugar_g, protein_g, vitaminA_pdv, vitaminC_pdv) VALUES
+('ground beef', 1, 'lb', 1506, 136, 304, 0, 65, 0, 0),
+('kidney beans', 1, 'cup', 613, 1.5, 44, 4, 43, 0, 13),
+('onion', 110, 'grams', 44, 0, 4, 5, 1, 0, 13),
+('tomato sauce', 1, 'cup', 70, 0, 27, 10, 3, 21, 28),
+('Krabby patty bun', 1, 'bun', 100, 1, 190, 2, 3, 0, 0),
+('lemon', 58, 'grams', 17, 0.2, 1, 1.5, .6, 0, 51),
+('sugar', 2.3, 'grams', 9, 0, 0, 2.3, 0, 0, 0),
+('water', 1, 'cup', 0, 0, 0, 0, 0, 0, 0);
+
+-- sample cookware
+INSERT INTO cookware (name, cost) VALUES
+('crockpot', 42.50),
+('cooking spoon', 5.99),
+('spatula', 6.99),
+('grill', 159.00),
+('drinking glass', 5.50);
+
+-- sample user_contributor
+INSERT INTO user_contributor (follower_id, contributor_id) VALUES (2, 1), (1, 2), (1, 4), (4, 3), (4, 2);
+
+-- sample saved_recipes
+INSERT INTO saved_recipes (user_id, recipe_id) VALUES (1, 1), (4, 1), (3, 2), (2, 3);
+
+-- sample recipe_cookware
+INSERT INTO recipe_cookware (recipe_id, cookware_id) VALUES (1, 1), (1, 2), (2, 3), (2, 4), (3, 5);
+
+-- sample recipe_ingredients
+INSERT INTO recipe_ingredients (recipe_id, ingredient_id, ingredient_quantity) VALUES (1, 1, .5), (1, 2, .75), (1, 3, 80), (1, 4, 1.5), (2, 5, 1), (2, 1, .25), (3, 6, 116), (3, 7, 20), (3, 8, 2);
---
+++ DMQ.sql
@@ -0,0 +1,116 @@
+/*
+    Title: Recipe Project Data Manipulation Queries
+    Author: Mark Piccirilli
+    Date: 7/19/2015
+    Course/Assignment: cs340/Project
+    Description: This file contains the data Manipulation queries to search or filter the database,
+    insert new information into the database, and gather any necessary information needed for
+    pages in the website.
+*/
+
+-- SEARCH/FILTER RECIPE QUERIES
+-- keyword search
+SELECT id FROM recipes where name=[name];
+
+-- search based on meal_type and/or ethnic_cuisine
+SELECT id FROM recipes WHERE meal_type=[meal_type] AND ethnic_cuisine=[ethnic_cuisine];
+
+-- search based on meal_type and/or ethnic_cuisine where low cal is selected
+
+-- search based on meal_type and/or ethnic_cuisine when low sodium is selected
+
+-- search based on meal_type and/or ethnic_cuisine when low_sodium and low cal are selected
+
+
+-- SEARCH FOR INGREDIENTS AND COOKWARE
+-- search ingredients
+SELECT id FROM ingredients WHERE name=[ingredient_name];
+-- after user select ingredient find serving_size_unit with this query
+SELECT serving_size_unit FROM ingredients WHERE id=[id];
+-- the prompt user to update the quantity feild(in serving size unit) of the recipe_ingredient table
+UPDATE recipe_ingredients SET ingredient_quantity=[quantity] WHERE id=[id];
+
+-- search cookware
+SELECT id FROM cookware WHERE name=[item_name];
+
+
+-- FIND HEALTH INFORMATION ABOUT A RECIPE
+-- calories
+SELECT (sum(i.calories*(ri.ingredient_quantity/i.serving_size)))/r.servings FROM ingredients i INNER JOIN recipe_ingredients ri ON i.id=ri.ingredient_id
+INNER JOIN recipes r ON r.id=ri.recipe_id AND r.id=[recipe_id];
+-- fat_g
+SELECT (sum(i.fat_g*(ri.ingredient_quantity/i.serving_size)))/r.servings FROM ingredients i INNER JOIN recipe_ingredients ri ON i.id=ri.ingredient_id
+INNER JOIN recipes r ON r.id=ri.recipe_id AND r.id=[recipe_id];
+-- sodium_mg
+SELECT (sum(i.sodium_mg*(ri.ingredient_quantity/i.serving_size)))/r.servings FROM ingredients i INNER JOIN recipe_ingredients ri ON i.id=ri.ingredient_id
+INNER JOIN recipes r ON r.id=ri.recipe_id AND r.id=[recipe_id];
+-- sugar_g
+SELECT (sum(i.sugar_g*(ri.ingredient_quantity/i.serving_size)))/r.servings FROM ingredients i INNER JOIN recipe_ingredients ri ON i.id=ri.ingredient_id
+INNER JOIN recipes r ON r.id=ri.recipe_id AND r.id=[recipe_id];
+-- protein_g
+SELECT (sum(i.protein_g*(ri.ingredient_quantity/i.serving_size)))/r.servings FROM ingredients i INNER JOIN recipe_ingredients ri ON i.id=ri.ingredient_id
+INNER JOIN recipes r ON r.id=ri.recipe_id AND r.id=[recipe_id];
+-- vitaminA_pdv
+SELECT (sum(i.vitaminA_pdv*(ri.ingredient_quantity/i.serving_size)))/r.servings FROM ingredients i INNER JOIN recipe_ingredients ri ON i.id=ri.ingredient_id
+INNER JOIN recipes r ON r.id=ri.recipe_id AND r.id=[recipe_id];
+-- vitaminC_pdv
+SELECT (sum(i.vitaminC_pdv*(ri.ingredient_quantity/i.serving_size)))/r.servings FROM ingredients i INNER JOIN recipe_ingredients ri ON i.id=ri.ingredient_id
+INNER JOIN recipes r ON r.id=ri.recipe_id AND r.id=[recipe_id];
+
+--INSERT QUERIES
+-- insert an ingredient (user adds a new ingredient)
+INSERT INTO ingredients (name, serving_size, serving_size_unit, calories, fat_g, sodium_mg, sugar_g, protein_g, vitaminA_pdv, vitaminC_pdv)
+VALUES ([name], [serving_size], [serving_size_unit], [calories], [fat_g], [sodium_mg], [sugar_g], [protein_g], [vitaminA_pdv], [vitaminC_pdv]);
+
+-- insert cookware (user adds a new cookware item)
+INSERT INTO cookware (name, price) VALUES ([name], [price]);
+
+-- insert a recipe
+-- recipe table (new recipe added by user)
+INSERT INTO recipes (name, instructions, meal_type, ethnic_cuisine, contributor, date_contributed) VALUES
+([name], [instructions], [meal_type], [ethnic_cuisine], [contributor], [date_contributed]);
+-- recipe_ingredients table (ingredients for user recipe)
+INSERT INTO recipe_ingredients (recipe_id, ingredient_id) VALUES ([recipe_id], [ingredient_id]);
+-- recipe_cookware table (cookware for user recipe)
+INSERT INTO recipe_cookware (recipe_id, cookware_id) VALUES ([recipe_id, cookware_id]);
+
+-- insert a user (new user created)
+INSERT INTO users (first_name, last_name, user_name, user_password, email, cooking_experience) VALUES
+([first_name], [last_name], [user_name], [user_password], [email], [cooking_experience]);
+
+-- insert new saved recipe (user saves a recipe)
+INSERT INTO saved_recipes (user_id, recipe_id) VALUES ([user_id], [recipe_id]);
+
+-- insert new followed user (user follows another user)
+INSERT INTO user_contributor (follower_id, contributor_id) VALUES ([follower_id], [contributor_id]);
+
+
+-- EDIT RECIPE QUERIES
+-- update recipes
+UPDATE recipes SET name=[name], instructions=[instructions], meal_type=[meal_type],
+ethnic_cuisine=[ethnic_cuisine] WHERE id=[recipe_id];
+
+-- delete recipe
+DELETE FROM recipes WHERE id=[recipe_id];
+
+
+-- CONTRIBUTOR PAGE QUERIES(can be viewed by all)
+-- find the appropriate account information to display on the page (name, user name, cooking experience)
+SELECT first_name, last_name, user_name, cooking_experience FROM users WHERE id=[id]
+-- find all recipes from a certain contributor
+SELECT id, name FROM recipes WHERE contributor=[contributor_id];
+
+
+-- ACCOUNT PAGE QUERIES(only viewed by user of that account)
+-- find all account information except password to display on page
+SELECT first_name, last_name, user_name, email, cooking_experience FROM users WHERE id=[id];
+
+-- find recipes contributed to
+SELECT id, name FROM recipes WHERE contributor = [contributor_id];
+
+-- update account info
+UPDATE users SET first_name=[first_name], last_name=[last_name], user_name=[user_name], password=[password],
+ email=[email], cooking_experience=[cooking_experience] WHERE id=[id];
+
+-- delete Account
+DELETE FROM users WHERE id=[user_id];
